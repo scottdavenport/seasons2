420ce8ce640f1d76c53bab1939650831d0f609e2
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-18 14:09:06 -0400

    move geolocation

--- src/index.js
+++ src/index.js
@@ -1,17 +1,12 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-// const App = () => {
-// 	window.navigator.geolocation.getCurrentPosition(
-// 		(position) => console.log(position),
-// 		(err) => console.log(err),
-// 	);
-
-// 	return <div>Hi There!</div>;
-// };
-
 class App extends React.Component {
 	render() {
+		window.navigator.geolocation.getCurrentPosition(
+			(position) => console.log(position),
+			(err) => console.log(err),
+		);
 		return <div>Latitude:</div>;
 	}
 }

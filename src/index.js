de15de6c317d57e358c2c7b7a4ce7491349ef349
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-18 15:44:24 -0400

    initialize state

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 class App extends React.Component {
+	constructor(props) {
+		super(props);
+
+		this.state = { lat: null };
+	}
+
 	render() {
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => console.log(position),

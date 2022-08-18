fa0c7e97173eff4d2af7247af54725f405ae25fd
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-18 14:07:49 -0400

    refactor App into class based component

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-const App = () => {
-	window.navigator.geolocation.getCurrentPosition(
-		(position) => console.log(position),
-		(err) => console.log(err),
-	);
+// const App = () => {
+// 	window.navigator.geolocation.getCurrentPosition(
+// 		(position) => console.log(position),
+// 		(err) => console.log(err),
+// 	);
 
-	return <div>Hi There!</div>;
-};
+// 	return <div>Hi There!</div>;
+// };
+
+class App extends React.Component {
+	render() {
+		return <div>Latitude:</div>;
+	}
+}
 
 createRoot(document.querySelector('#root')).render(<App />);

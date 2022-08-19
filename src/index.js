a5c0b6bacb2911a925bdd03d1a6247037516006b
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-19 10:25:20 -0400

    setting state

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ class App extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: '' };
 
 		window.navigator.geolocation.getCurrentPosition(
 			(position) => {
 				this.setState({ lat: position.coords.latitude });
 			},
-			(err) => console.log(err),
+			(err) => {
+				this.setState({ errorMessage: err.message });
+			},
 		);
 	}
 

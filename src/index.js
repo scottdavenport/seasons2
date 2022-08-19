f75503aeb5692c58818aa12d0ba6eb092199823e
Scott Davenport <sdavenport@shopmonkey.io>
2022-08-19 13:59:28 -0400

    start working on SeasonDisplay component and passing it state

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import SeasonDisplay from './SeasonDisplay';
 
 class App extends React.Component {
 	state = { lat: null, errorMessage: '' };
@@ -17,7 +18,7 @@ class App extends React.Component {
 		}
 
 		if (!this.state.errorMessage && this.state.lat) {
-			return <div>Latitude: {this.state.lat}</div>;
+			return <SeasonDisplay lat={this.state.lat} />;
 		}
 
 		return <div>Loading!</div>;

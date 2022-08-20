c8e045ae510bb5ac15025d1f034703d34dfe3e86
scottdavenport <scott@pinpoint.com>
2022-08-20 08:11:36 -0400

    add icons for summer and winter

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -10,7 +10,16 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
 	const season = getSeason(props.lat, new Date().getMonth());
-	return <div>Latitude: {season}</div>;
+	const text = season === 'winter' ? 'Brrr it is chilly' : 'Lets hit the beach';
+	const icon = season === 'winter' ? 'snowflake' : 'sun';
+
+	return (
+		<div>
+			<i className={`${icon} icon`} />
+			<h1>{text}</h1>
+			<i className={`${icon} icon`} />
+		</div>
+	);
 };
 
 export default SeasonDisplay;
